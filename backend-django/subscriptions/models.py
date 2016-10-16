fe23b9bfdc640d6888c375635f1d5fac52ac6ef4
Yoel Monzon <yoelfme@hotmail.com>
2016-10-16 02:49:21 -0600

    [add] new app subscriptions, with models and migrations

---
+++ backend-django/subscriptions/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible, smart_text
+
+@python_2_unicode_compatible
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    payment_date = models.DateTimeField()
+    stripe_id = models.CharField(max_length=25)
+    stripe_subscription_id = models.CharField(max_length=25, blank=True)
+
+    def __str__(self):
+        return smart_text("{0} - {1}".format(self.name, self.email))
